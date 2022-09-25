bab437dc9341c09928a9d7e7e5c42d9accbbec9d
rachel-o-donnell <hellotherachelo@gmail.com>
2022-09-25 15:59:12 +0000

    Randomly choose a word based on the set conditions

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+from words import words
+
+def choose_valid_word(words):
+    """ 
+    Eliminates words that are less than 6 characters long, have spaces or have hyphens
+    Randomly chooses a word for our game.
+    """
+    word_in_play = random.choice(words)
+    while ' ' in word_in_play or '-' in word_in_play or len(word_in_play) < 6:
+         word_in_play = random.choice(words)
+    print(word_in_play)
+
+
+
+choose_valid_word(words)
