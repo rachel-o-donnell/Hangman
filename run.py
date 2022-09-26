c8463d1d8aa40b11e6bc59e44ad96ee1f39d2c25
rachel-o-donnell <hellotherachelo@gmail.com>
2022-09-26 16:31:35 +0000

    Add letter validation to start_game and tidy up terminal display

--- run.py
+++ run.py
@@ -12,14 +12,22 @@ def choose_valid_word(words):
     word_in_play = random.choice(words)
     while ' ' in word_in_play or '-' in word_in_play or len(word_in_play) < 6:
          word_in_play = random.choice(words)
+         return(word_in_play)
 
 def start_game():
+    """
+    Prints opening message to the terminal, asks player to input a letter,
+    checks that the answer is a letter and returns this chosen letter
+    """
     print("Welcome to Hangman, you have 7 tries to guess the correct letters or you will be hanged\n")
-    print (f"These are the available letters: {available_letters}\n")
-    input("Choose a letter ")
+    print (f"These are the available letters:\n {available_letters}\n")
+    chosen_letter = input("Choose a letter: ").upper()
+    if chosen_letter.isalpha():
+        print (f"You chose letter {chosen_letter}")
+    else: 
+        print("Invalid answer, you must choose a letter\n")
+    return chosen_letter
     
-    return(word_in_play)
-
 
 
 available_letters = sorted(set(string.ascii_uppercase))
@@ -27,6 +35,9 @@ used_letters = set()
 lives = 7 
 guessing_area = ''
 
+
 start_game()
+
+
 word = choose_valid_word(words)
-identify_letter()
+
